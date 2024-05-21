f52651030667daed488be451310db52ad69b78c0
junaidtk <junaid9037@gmail.com>
2024-05-21 19:09:40 +0530

    added template engin

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const expressEdge = require("express-edge");
 const path = require("path");
 
 const app = new express();
 
 app.use(express.static("public"));
+app.use(expressEdge);
+app.set("views", `${__dirname}/views`);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
