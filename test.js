be671ee1785eb149a7f0eab4b715e64d6da999aa
junaidtk <junaid9037@gmail.com>
2024-05-23 21:19:09 +0530

    different db operation, create posts template index.js modification

--- test.js
+++ test.js
@@ -5,6 +5,15 @@ const { error } = require("console");
 
 mongoose.connect("mongodb://localhost/node-js-test-blog");
 
+// Get all posts
+async function findAllPost() {
+  const findpost = await Post.find({});
+  console.log(findpost);
+}
+
+// findAllPost();
+
+// Find sepecific item with searched title
 async function findPost() {
   const findpost = await Post.find({ title: "second junu title" });
   console.log(findpost);
@@ -12,6 +21,7 @@ async function findPost() {
 
 // findPost();
 
+// Create Post
 async function ceatePost() {
   var createdPost = await Post.create({
     title: "third junu title",
@@ -24,12 +34,22 @@ async function ceatePost() {
 
 // ceatePost();
 
+// Get specific Item by using iD
 async function getSpecificItem() {
   var item = await Post.findById("664f5955b664ca79cac77dd5");
   console.log(item);
 }
 
-getSpecificItem();
+// getSpecificItem();
+
+async function updatePosts() {
+  const update = await Post.findByIdAndUpdate("664f4334f85076fcc67769b2", {
+    title: "initial posts",
+  });
+  console.log(update);
+}
+
+updatePosts();
 
 // Post.create({
 //   title: "junu title",
