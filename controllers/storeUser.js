2974132010d498196002aecc174fc3ba734a1e6c
junaidtk <junaid9037@gmail.com>
2024-05-29 21:08:42 +0530

    password hashing

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -2,9 +2,10 @@ const User = require("../models/user");
 
 module.exports = (req, res) => {
   async function createUser() {
+    console.log("created user controller");
     const createdUser = await User.create(req.body);
-
-    console.log(createdUser);
+    console.log("created user enddddd");
+    //console.log(createdUser);
 
     res.redirect("/");
   }
--- models/user.js
+++ models/user.js
@@ -1,9 +1,20 @@
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
+});
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+
+  const encrypted = await bcrypt.hash(user.password, 10);
+
+  user.password = encrypted;
+
+  next();
 });
 
 module.exports = mongoose.model("user", UserSchema);
